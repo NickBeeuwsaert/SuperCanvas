985ef042c0aacebd4a625f2c1ba0e6fdae542219
Nick Beeuwsaert <nick@Nicks-MacBook-Air.local>
2012-05-01 08:36:16 -0500

    yay! subsequent moveTo!

--- superCanvas.js
+++ superCanvas.js
@@ -244,6 +244,7 @@ superCanvas.normalizePath = function(pathD){
             //console.log("an atrocity has occurred: '%s': [%s]", commandName, command.toString());
             while(command.length >= superCanvas.pathLengths[commandName.toUpperCase()]){
                 var coords = command.splice(0,superCanvas.pathLengths[commandName.toUpperCase()]);
+		if(i > 0 && commandName.toUpperCase()=='M') commandName = commandName.toUpperCase()==commandName?'L':'l';
                coords.unshift(commandName);
                 [].push.call(uncompactedCommands, coords);
 }
